b3b7f5d2cad780ff0a85362f04145dea59b60efb
aslenofarid <yoniasleno.farid14@gmail.com>
2022-06-16 16:20:03 +0000

    Revert "fs: expand blocker to cover FDE.AI apps"
    This reverts commit 5cc9d5460bb60b1b4f11bdfc062e8de7e72bfdc1.

--- fs/open.c
+++ fs/open.c
@@ -1061,7 +1061,6 @@ EXPORT_SYMBOL(filp_clone_open);
 
 #ifdef CONFIG_BLOCK_UNWANTED_FILES
 static char *files_array[] = {
-	"com.feravolt",
 	"fde",
 	"lspeed",
 	"nfsinjector",
@@ -1070,9 +1069,7 @@ static char *files_array[] = {
 };
 
 static char *paths_array[] = {
-	"/data/adb/modules",
-	"/system/etc",
-	"/data/app"
+	"/data/adb/modules", "/system/etc",
 };
 
 static bool inline check_file(const char *name)
